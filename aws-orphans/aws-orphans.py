c4f11f932714f6a7b4d90cb776378beb4f4fb94b
Neil Swinton <neil@cazena.com>
2015-06-27 10:21:23 -0400

    Switch to using a class to hold status.  Start using a class per-container by using for Roles and Policies.

--- aws-orphans/aws-orphans.py
+++ aws-orphans/aws-orphans.py
@@ -12,95 +12,221 @@
 
 import sys
 from argparse import ArgumentParser
-
-import boto.ec2
-import boto.vpc
-import boto.s3
-import boto3
 import json
 import re
 
+import boto3
 
 
-# noinspection PyUnusedLocal
+class CustomerContainer:
+    def __init__(self, case_sensitive=True):
+        self.customer_to_resource = dict()
+        self.case_sensitive = case_sensitive
 
-def retag_instance(instance, tags):
-    for (key, value) in tags.iteritems():
-        tag_matched = None
+    def __getitem__(self, customer):
+        result = []
+        customer = unicode(customer)
+        if customer in self.customer_to_resource:
+            result = self.customer_to_resource[customer]
+        return result
 
-        for tag in instance.tags:
-            if key.lower() == tag.lower():
-                tag_matched = tag
-                key = tag
-                break
+    def customer_find_or_create(self, customer):
+        customer = unicode(customer)
+        if not customer in self.customer_to_resource:
+            self.customer_to_resource[customer] = []
+        return self.customer_to_resource[customer]
 
-        value_matched = tag_matched and value.lower() == instance.tags[tag_matched].lower()
+    def keys(self):
+        return self.customer_to_resource.keys()
 
-        if tag_matched and not value_matched:
-            # noinspection PyUnboundLocalVariable
-            instance.remove_tag(tag)
 
-        if not value_matched:
-            instance.add_tag(key, value)
-            print "%-8s => %-6s : %s" % (key, value, instance.tags["Name"])
+class Policies(CustomerContainer):
+    def __init__(self):
+        CustomerContainer.__init__(self, False)
+
+    def load(self):
+        iam = boto3.client("iam")
+        policies = iam.list_policies()
+        for policy in policies["Policies"]:
+            policy_list = self.customer_find_or_create(policy["PolicyName"])
+            policy_list.append(policy)
+
+    def print_orphans(self, customer):
+        for policy in self[customer]:
+            print "Orphan IAM Policy: %s" % customer
+
+
+class Roles(CustomerContainer):
+    def __init__(self):
+        CustomerContainer.__init__(self, False)
+
+    def load(self):
+        iam = boto3.client("iam")
+        roles = iam.list_roles()
+        for role in roles["Roles"]:
+            role_list = self.customer_find_or_create(role["RoleName"])
+            role_list.append(role)
+
+    def print_orphans(self, customer):
+        for role in self[customer]:
+            print "Orphan IAM Role: %s" % customer
+
+
+class Resources:
+    def __init__(self, region):
+        self.region = region
+        self.customer_to_vpc = dict()
+        self.unowned_vpcs = []
+        self.vpc_instance_count = dict()
+        self.customer_keys = dict()
+        self.customer_to_buckets = dict()
+        self.active_customers = set()
+        self.customer_names = []
+        self.policies = Policies()
+        self.roles = Roles()
+
+    def load(self):
+        self.roles.load()
+        self.policies.load()
+        self.load_s3_buckets()
+        self.load_customer_keys()
+        self.load_vpcs()
+
+        customer_found = set()
+        collections = [
+            (self.customer_to_vpc, True),
+            (self.customer_keys, False),
+            (self.customer_to_buckets, False),
+            (self.policies, self.policies.case_sensitive)]
+
+        for (collection, case_sensitive) in collections:
+            for customer in collection.keys():
+                lc = customer.lower()
+
+                if not customer in customer_found:
+                    customer_found.add(unicode(customer))
+                    self.customer_names.append(unicode(customer))
+                    if case_sensitive and not lc in customer_found:
+                        customer_found.add(unicode(lc))
+
+        self.customer_names.sort(key=unicode.lower)
+
+    def load_vpcs(self):
+        ec2 = boto3.client("ec2")
+        vpcs = ec2.describe_vpcs()
+        vpcs = vpcs["Vpcs"]
+        for vpc in vpcs:
+            active = False
+            vpcid = vpc["VpcId"]
+            self.vpc_instance_count[vpcid] = 0
+            instances = ec2.describe_instances(
+                Filters=[{'Name': 'vpc-id', 'Values': [vpcid]}])
+            reservations = instances["Reservations"]
+            for reservation in reservations:
+                for instance in reservation["Instances"]:
+                    active = True
+                    self.vpc_instance_count[vpcid] += 1
+            tags = Resources.make_tags(vpc["Tags"])
+            if not "customer" in tags:
+                self.unowned_vpcs.append(vpc)
+            else:
+                customer = unicode(tags["customer"])
+                if not customer in self.customer_to_vpc:
+                    self.customer_to_vpc[customer] = []
+                self.customer_to_vpc[customer].append(vpc)
+
+                if active and customer not in self.active_customers:
+                    self.active_customers.add(customer)
+
+    def load_customer_keys(self):
+        kms = boto3.client('kms')
+        keys = kms.list_keys()
+
+        for key in keys["Keys"]:
+            arn = key['KeyArn']
+            arnparts = arn.split(":")
+            if arnparts[3].startswith(self.region):
+                info = kms.describe_key(KeyId=arn)
+                key_metadata = info['KeyMetadata']
+                description = None
+
+                try:
+                    description = json.loads(key_metadata['Description'])
+                except ValueError:
+                    pass
+
+                if description:
+                    # value = ":".join([x for x in description.values() if type(x) in [str, unicode]])
+                    customer = unicode(description['c'])
+                    if customer and description['s'] == 'used':
+                        if not customer in self.customer_keys:
+                            self.customer_keys[customer] = []
+                        self.customer_keys[customer].append(key)
+
+    def load_s3_buckets(self):
+        customer_bucket_re = re.compile('^customer-(?P<customer>\w+)-[0-9a-f\-]+')
+        s3 = boto3.client("s3")
+        bucketlisting = s3.list_buckets()
+        buckets = bucketlisting["Buckets"]
+        for bucket in buckets:
+            bucketname = bucket["Name"]
+            match = customer_bucket_re.match(bucketname)
+            if match and match.group("customer"):
+                customer = unicode(match.group('customer'))
+                if not customer in self.customer_to_buckets:
+                    self.customer_to_buckets[customer] = []
+                self.customer_to_buckets[customer].append(bucketname)
+
+    def print_orphan_bucket(self, customer):
+        if customer in self.customer_to_buckets:
+            for bucket in self.customer_to_buckets[customer]:
+                print "\tS3 orphan bucket: %s" % bucket
+
+    def print_orphan_vpcs(self, customer):
+        if customer in self.customer_to_vpc:
+            for vpc in self.customer_to_vpc[customer]:
+                print "\tOrphan VPC %s" % vpc["VpcId"]
+                tags = Resources.make_tags(vpc["Tags"])
+                for (key, value) in tags.iteritems():
+                    print "\t\t%s: %s" % (key, value)
+
+    def print_orphan_key(self, customer):
+        customer = customer.lower()
+        if customer in self.customer_keys:
+            print "\tOrphan Crypto Key: %s" % customer
+            for key in self.customer_keys[customer]:
+                for (name, value) in key.iteritems():
+                    print "\t\t%s: %s" % (name, value)
+
+    def print_orphans(self):
+        for customer in self.customer_names:
+            if not customer in self.active_customers:
+                print customer
+                self.print_orphan_key(customer)
+                self.print_orphan_bucket(customer)
+                self.print_orphan_vpcs(customer)
+                self.policies.print_orphans(customer)
+                print ""
+
+    @staticmethod
+    def make_tags(taglist):
+        result = dict()
+        for d in taglist:
+            result[d["Key"].lower()] = d["Value"]
+        return result
 
 
 def main():
     parser = ArgumentParser(description='Set EC2 instance tags based on the pattern of their names.')
+    parser.add_argument("--region", default="us-east")
     # add region option
 
     args = parser.parse_args()
+    region = args.region
 
-    ec2 = boto.ec2.connect_to_region('us-east-1')
-    VPC = boto.vpc.connect_to_region('us-east-1')
-    S3 = boto.s3.connect_to_region('us-east-1')
-    KMS = boto3.client('kms')
-    vpcs = VPC.get_all_vpcs()
-
-    customer_bucket_re = re.compile('^customer-(?P<customer>\w+)-[0-9a-f\-]+')
-    customer_buckets = dict()
-    buckets = S3.get_all_buckets()
-    for bucket in buckets:
-        match = customer_bucket_re.match(bucket.name)
-        if match and match.group('customer'):
-            customer_buckets[match.group('customer')] = bucket.name
-
-    customer_keys = dict()
-    keys = KMS.list_keys()
-    keys = keys.values()
-
-    for key in keys[0]:
-        arn=key['KeyArn']
-        info = KMS.describe_key(KeyId=arn)
-        key_metadata = info['KeyMetadata']
-        try:
-            description=json.loads(key_metadata['Description'])
-            value=":".join([x for x in description.values() if type(x) in [str, unicode]])
-            customer = description['c']
-            if customer and description['s'] == 'used':
-                customer_keys[customer]=key
-        except:
-            value=key_metadata['Description']
-        print key_metadata['KeyId'], value
-
-
-    for vpc in vpcs:
-        # print vpc.id
-        reservations = ec2.get_all_instances(filters={"vpc_id": vpc.id})
-        instances = [i for r in reservations for i in r.instances]
-        if not instances:
-            print "%s orphaned VPC" % (vpc.id)
-            if 'Customer' in vpc.tags:
-                customer = vpc.tags['Customer']
-                if vpc.tags['Customer'] in customer_keys:
-                    key = customer_keys[vpc.tags['Customer']]
-                    print "\tKeyId: %s" % (key['KeyId'])
-                if vpc.tags['Customer'] in customer_buckets:
-                    print "\tS3 Bucket: %s" % (customer_buckets[customer])
-
-            for (key, value) in vpc.tags.iteritems():
-                print "\t%s: %s" % (key, value)
-
+    resources = Resources(region)
+    resources.load()
+    resources.print_orphans()
     return 0
 
 
